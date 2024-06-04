a2f2b7e40f302f5d7a15756b15a64aad3ba371b1
Varalu N <108639268+nVarap@users.noreply.github.com>
2024-06-03 22:07:37 -0700

    Hiding Secrets :(

--- assets/javascript/firebaseSetup.js
+++ assets/javascript/firebaseSetup.js
@@ -8,13 +8,13 @@ import { getFirestore, collection, addDoc } from "https://www.gstatic.com/fireba
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCpXVBDw4v0T8twSBDbmOJ4ZtvBpA4-nzk",
-  authDomain: "public-site-4c414.firebaseapp.com",
-  projectId: "public-site-4c414",
-  storageBucket: "public-site-4c414.appspot.com",
-  messagingSenderId: "364927581873",
-  appId: "1:364927581873:web:d148a8ade25f7c559fac90",
-  measurementId: "G-50695YL6BF"
+  apiKey: API-KEY,
+  authDomain: DOMAIN,
+  projectId: ID,
+  storageBucket: BUCKET,
+  messagingSenderId: SENDER-ID,
+  appId: APP-ID,
+  measurementId: M-ID
 };
 
 // Initialize Firebase
